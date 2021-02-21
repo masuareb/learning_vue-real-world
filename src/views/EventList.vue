70239e403e65e89d4c4415dda9c055c34e0c6d4b
Arturo Suárez <masuareb@gmail.com>
2021-02-21 16:00:18 -0600

    Add and display event card component.

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,9 +1,19 @@
 <template>
   <div>
     <h1>Events Listing</h1>
+    <EventCard/>
     <p>
       <router-link :to="{ name: 'event-show', params: { id: '1' }}">First Event</router-link>
     </p>
   </div>
 
 </template>
+
+<script>
+import EventCard from "@/components/EventCard";
+export default {
+  components: {
+    EventCard
+  }
+}
+</script>
