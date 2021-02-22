f84e667b83742917ddf8ec7604e496d38ecae67c
Arturo Suárez <masuareb@gmail.com>
2021-02-21 18:45:44 -0600

    Make AXIOS call to get the list of events.

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard/>
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
     <p>
     </p>
   </div>
@@ -10,9 +10,25 @@
 
 <script>
 import EventCard from "@/components/EventCard";
+import axios from 'axios'
 export default {
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    axios
+      .get('http://localhost:3000/events')
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        console.og('There was an error:', error.response)
+      })
   }
 }
 </script>
